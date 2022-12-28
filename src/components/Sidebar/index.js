0fb22be4e19762f2df4644581be3793bb9792316
Khushboo Gandhi <khushboo1028@gmail.com>
2022-12-27 17:50:17 -0800

    removed blog

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import { IconContext } from 'react-icons';
-import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink } from './SidebarElements';
+import React from "react";
+import { IconContext } from "react-icons";
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink
+} from "./SidebarElements";
 
 const Sidebar = ({ isOpen, toggle }) => {
-	return (
-		<IconContext.Provider value={{ color: '#021C35' }}>
-			<SidebarContainer isOpen={isOpen} onClick={toggle}>
-				<Icon onClick={toggle}>
-					<CloseIcon />
-				</Icon>
-				<SidebarWrapper>
-					<SidebarMenu style={{ margin: 'auto', width: '50%', padding: '10px' }}>
-						<SidebarLink onClick={toggle} to="/">
-							Home
-						</SidebarLink>
-						<SidebarLink onClick={toggle} to="/work">
-							Work
-						</SidebarLink>
-						<SidebarLink onClick={toggle} to="/blog">
-							Blog
-						</SidebarLink>
-						<SidebarLink onClick={toggle} to="/connect">
-							Connect
-						</SidebarLink>
-					</SidebarMenu>
-				</SidebarWrapper>
-			</SidebarContainer>
-		</IconContext.Provider>
-	);
+  return (
+    <IconContext.Provider value={{ color: "#021C35" }}>
+      <SidebarContainer isOpen={isOpen} onClick={toggle}>
+        <Icon onClick={toggle}>
+          <CloseIcon />
+        </Icon>
+        <SidebarWrapper>
+          <SidebarMenu
+            style={{ margin: "auto", width: "50%", padding: "10px" }}
+          >
+            <SidebarLink onClick={toggle} to="/">
+              Home
+            </SidebarLink>
+            <SidebarLink onClick={toggle} to="/work">
+              Work
+            </SidebarLink>
+            <SidebarLink onClick={toggle} to="/connect">
+              Connect
+            </SidebarLink>
+          </SidebarMenu>
+        </SidebarWrapper>
+      </SidebarContainer>
+    </IconContext.Provider>
+  );
 };
 
 export default Sidebar;
